3d606cd765b4dd373cce0ecb16ff05c6188cea7f
Anthony Williams <wolven531@Anthonys-MBP.local>
2017-07-07 17:31:31 -0700

    Updated namespace properly

--- Assets/Scripts/ImgDisp.cs
+++ Assets/Scripts/ImgDisp.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace awill {
+namespace awillInc {
     public class ImgDisp : MonoBehaviour
     {
         /// <summary>
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using awill;
+using awillInc;
 
 public class Main : MonoBehaviour {
 	public ImgDisp IMG_PREFAB;
